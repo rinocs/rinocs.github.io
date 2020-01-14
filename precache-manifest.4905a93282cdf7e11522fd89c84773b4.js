76111ac0cd8e5683b55731b0bf8a07cc69937562
rinocs <rinocs92@gmail.com>
2020-01-14 17:17:21 +0100

    new version

---
+++ precache-manifest.4905a93282cdf7e11522fd89c84773b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "03e74288add5637a6e19",
+    "url": "/static/js/main.03e74288.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "03e74288add5637a6e19",
+    "url": "/static/css/main.963446a5.chunk.css"
+  },
+  {
+    "revision": "36ed03aac095bd7f3249b88742a59221",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b7a1a093f45ecd5f712987893d87cb9c.js"
+  "/precache-manifest.4905a93282cdf7e11522fd89c84773b4.js"
 );
 
 workbox.clientsClaim();
